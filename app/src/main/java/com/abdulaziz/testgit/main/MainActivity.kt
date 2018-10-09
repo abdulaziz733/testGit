9306acc0b31e8b43ac9e34aa0812e5d4a51a469e
abdulaziz733 <abdulaziz733@gmail.com>
2018-10-09 10:18:07 +0700

    Revert "Revert "add module main3""
    This reverts commit 9e89df6b330ea8d45549df38dd5b164c0f0f1148.

--- app/src/main/java/com/abdulaziz/testgit/main/MainActivity.kt
+++ app/src/main/java/com/abdulaziz/testgit/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.abdulaziz.testgit.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.abdulaziz.testgit.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    private fun test (){
+        Toast.makeText(this, "test 123 123", Toast.LENGTH_SHORT).show()
     }
 
 }
