7d40aae1ac63689b26a4d4f5be88e15a4475b86b
abdulaziz733 <abdulaziz733@gmail.com>
2018-10-09 10:05:04 +0700

    add module m2

---
+++ app/src/main/java/com/abdulaziz/testgit/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.abdulaziz.testgit.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.abdulaziz.testgit.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
---
+++ app/src/main/java/com/abdulaziz/testgit/main2/mainActivity2.kt
@@ -0,0 +1,13 @@
+package com.abdulaziz.testgit.main2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.abdulaziz.testgit.R
+
+class mainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
