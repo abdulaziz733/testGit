02466e5e728118b6541717b5698997fb8b4d8c11
abdulaziz733 <abdulaziz733@gmail.com>
2018-10-09 11:38:37 +0700

    add fa

---
+++ app/src/main/java/com/abdulaziz/testgit/FaActivity.kt
@@ -0,0 +1,12 @@
+package com.abdulaziz.testgit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fa)
+    }
+}
