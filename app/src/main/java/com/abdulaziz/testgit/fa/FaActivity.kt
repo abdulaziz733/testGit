c3044afd90ccd8126768bf4b70b578d91e416971
abdulaziz733 <abdulaziz733@gmail.com>
2018-10-30 09:57:55 +0700

    change activity

--- app/src/main/java/com/abdulaziz/testgit/fa/FaActivity.kt
+++ app/src/main/java/com/abdulaziz/testgit/fa/FaActivity.kt
@@ -2,12 +2,19 @@ package com.abdulaziz.testgit.fa
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.abdulaziz.testgit.R
 
 class FaActivity : AppCompatActivity() {
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fa)
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
     }
 }
